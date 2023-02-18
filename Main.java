1da95c36ac47fb880aa82e315167b2c4fd6dbda1
Subhadhip Karmakar <karmakarsubhadip59@gmail.com>
2023-02-18 13:10:49 +0530

    Changes to Odd

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args) {
         int[] numbs = {1, 2, 3, 4, 5};
         for(int numb: numbs) {
-            if(numb % 2 == 0) {
+            if(numb % 2 == 1) {
                 System.out.println(numb);
             }
         }
