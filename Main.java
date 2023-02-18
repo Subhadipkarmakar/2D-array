19349ece1835246a29a8762c1c0eacbb2a7baf2f
Subhadhip Karmakar <karmakarsubhadip59@gmail.com>
2023-02-18 13:08:57 +0530

    First Commit, added, print even numbs in Array

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int[] numbs = {1, 2, 3, 4, 5};
+        for(int numb: numbs) {
+            if(numb % 2 == 0) {
+                System.out.println(numb);
+            }
+        }
+    }
+}
